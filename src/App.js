08de9ae842419e2f119d4d645f01d5ec73de01b1
Musabii Mambetov <musabi@live.com>
2022-07-12 02:25:18 +0300

    getters

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
-const Search = ({value,onChange,children}) => (
+const Search = ({ value, onChange, children }) => (
   <div>
-    <label htmlFor='search' >{children}</label>
-    <input id='search' type='text' value={value} onChange={onChange}/>
+    <label htmlFor="search">{children}</label>
+    <input id="search" type="text" value={value} onChange={onChange} placeholder='search...'/>
   </div>
-)
+);
 
 function App() {
-
-  const [search,setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const handleChange = (e) => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value);
+  };
 
   return (
     <div className="App">
-     <Search value = {search} onChange={handleChange}>
+      <img alt="search image"/>
+      <Search value={search} onChange={handleChange}>
         Search:
-     </Search>
-    <p>Searches for {search?search:'...'}</p>
+      </Search>
+      <p>Searches for {search ? search : "..."}</p>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,10 @@ describe('App', () => {
     render(<App/>)
     expect(screen.getByText(/Search:/i)).toBeInTheDocument();
     expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/search.../i)).toBeInTheDocument();
+    expect(screen.getByAltText(/search image/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue('')).toBeInTheDocument();
   })
 
  })
