1da75cdcb5d487594be38abe14852bc61ff8900a
Musabii Mambetov <musabi@live.com>
2022-07-17 23:34:18 +0300

    select focus

--- src/App.test.js
+++ src/App.test.js
@@ -22,75 +22,98 @@ describe("App", () => {
     expect(await screen.findByText(/Logged in as/i)).toBeInTheDocument();
   });
 
-
   it("fire Event", async () => {
     render(<App />);
     await screen.findByText(/Logged in as/i);
-    screen.debug()
+    screen.debug();
     fireEvent.change(screen.getByRole("textbox"), {
       target: { value: "React" },
     });
-    screen.debug()
+    screen.debug();
   });
 
   it("user Event", async () => {
     render(<App />);
     await screen.findByText(/Logged in as/i);
-    screen.debug()
-    userEvent.type(screen.getByRole('textbox'),'React')
-    screen.debug()
+    screen.debug();
+    userEvent.type(screen.getByRole("textbox"), "React");
+    screen.debug();
   });
-
 });
 
-
-
-describe('first', () => {
-
-  it('checkbox fireEvent',()=>{
-
+describe("first", () => {
+  it("checkbox fireEvent", () => {
     const handleChange = jest.fn();
-    const {container} = render(<input type='checkbox' onChange={handleChange} />);
+    const { container } = render(
+      <input type="checkbox" onChange={handleChange} />
+    );
     const checkbox = container.firstChild;
 
     expect(checkbox).not.toBeChecked();
-    fireEvent.click(checkbox)
-    expect(handleChange).toBeCalled()
+    fireEvent.click(checkbox);
+    expect(handleChange).toBeCalled();
     expect(checkbox).toBeChecked();
+  });
 
-  })
-
-  it('checkbox userEvent',()=>{
-
+  it("checkbox userEvent", () => {
     const handleChange = jest.fn();
-    const {container} = render(<input type='checkbox' onChange={handleChange} />);
+    const { container } = render(
+      <input type="checkbox" onChange={handleChange} />
+    );
     const checkbox = container.firstChild;
 
     expect(checkbox).not.toBeChecked();
-    userEvent.click(checkbox)
-    expect(handleChange).toBeCalled()
+    userEvent.click(checkbox);
+    expect(handleChange).toBeCalled();
     expect(checkbox).toBeChecked();
+  });
 
-  })
-
-
-  it('checkbox userEvent double click',()=>{
-
+  it("checkbox userEvent double click", () => {
     const onChange = jest.fn();
-    const {container} = render(<input type='checkbox' onChange={onChange} />);
+    const { container } = render(<input type="checkbox" onChange={onChange} />);
     const checkbox = container.firstChild;
     expect(checkbox).not.toBeChecked();
-    userEvent.dblClick(checkbox)
-    expect(onChange).toHaveBeenCalledTimes(2)
+    userEvent.dblClick(checkbox);
+    expect(onChange).toHaveBeenCalledTimes(2);
+  });
 
-  })
+  it("input field", () => {
+    const { container } = render(<input type="text" className="someInput" />);
+    const input = container.getElementsByClassName("someInput");
+    expect(input).not.toBeNull();
+  });
 
-  it('input field',()=>{
-    const {container} = render(<input type='text' className="someInput"/>);
-    const input = container.getElementsByClassName('someInput');
-    expect(input).not.toBeNull()
+  it("focus", () => {
+    const { getAllByTestId } = render(
+      <div>
+        <input data-testid="element" type="checkbox" />
+        <input data-testid="element" type="radio" />
+        <input data-testid="element" type="number" />
+      </div>
+    );
 
-  })
+    const [checkbox, radio, number] = getAllByTestId("element");
+    userEvent.tab();
+    expect(checkbox).toHaveFocus();
+    userEvent.tab();
+    expect(radio).toHaveFocus();
+    userEvent.tab();
+    expect(number).toHaveFocus();
+  });
 
- })
+  it("select", () => {
+    const { selectOptions, getByRole, getByText } = render(
+      <select> //with mustiple attribute role become combolist
+        <option value="1">A</option>
+        <option value="2">B</option>
+        <option value="3">C</option>
+      </select>
+    );
+    userEvent.selectOptions(getByRole("combobox"), "1");
+    expect(getByText('A').selected).toBeTruthy();
 
+    userEvent.selectOptions(getByRole("combobox"), "2");
+    expect(getByText('B').selected).toBeTruthy();
+    expect(getByText('A').selected).toBeFalsy();
+  });
+});
